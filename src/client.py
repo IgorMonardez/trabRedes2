bbff9cdfe2a3cee0167a5173a0e67bff4b4a6cc7
caiovelp <cparanhos@id.uff.br>
2023-10-25 10:21:25 -0300

    Cliente pode se desvincular do servidor, e o servidor responde com a mensagem de encerramento

--- src/client.py
+++ src/client.py
@@ -1,23 +1,25 @@
 import socket
 
-# Criação do socket do cliente
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# Define o endereço e porta do servidor
-server_address = ('localhost', 12345)
-
-# Nome do cliente e porta para receber chamadas
-client_name = "Vasco"
-client_port = 7777
-
-# Conecta ao servidor
-client_socket.connect(server_address)
-
 while True:
+    # Criação do socket do cliente a cada iteração
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Define o endereço e porta do servidor
+    server_address = ('localhost', 12346)
+
+    # Nome do cliente e porta para receber chamadas
+    client_name = "Vasco"
+    client_port = 7777
+
+    # Conecta ao servidor
+    client_socket.connect(server_address)
+
     # Exibe um menu para o cliente escolher a ação
     print("Escolha uma ação:")
     print("1. Registrar-se no servidor")
     print("2. Realizar consulta de usuário")
+    print("3. Solicitar desvinculação do servidor")
+    print("4. Sair")
 
     choice = input("Opção: ")
 
@@ -34,8 +36,16 @@ while True:
         client_socket.send(query_request.encode())
         response = client_socket.recv(1024).decode()
         print("Resposta do servidor:", response)
+    elif choice == "3":
+        # Opção 3: Solicitar desvinculação do servidor
+        client_socket.send("UNREGISTER".encode())
+        print("Desvinculação do servidor solicitada.")
+        break
+    elif choice == "4":
+        # Opção 4: Sair
+        break
     else:
         print("Opção inválida. Tente novamente.")
 
-# Fecha o socket do cliente
-client_socket.close()
+    # Fecha o socket do cliente após cada iteração
+    client_socket.close()
