a5fcc719c9fe291cb131c8fd3302a10287fe0c1c
Igor de Paula Siqueira Monárdez <igormonardez@id.uff.br>
2023-11-23 18:26:36 -0300

    reverter os commits desnecessários

---
+++ entrega_2/client.py
@@ -0,0 +1,65 @@
+import select
+import socket
+from vidstream import StreamingServer, CameraClient
+import cv2
+
+def aguardando_video_call(server_socket):
+    intervalo = 5
+    tempo_restante = 20
+
+    print(f"Timer iniciado para {tempo_restante} segundos.")
+
+    while tempo_restante > 0:
+        ready, _, _ = select.select([server_socket], [], [], 1)  #  Espera por 1 segundo
+        if ready:
+            response = server_socket.recv(4096).decode()
+            if response:
+                return response
+        else:
+            if tempo_restante % intervalo == 0:
+                print(f"{tempo_restante} segundos restantes...")
+            tempo_restante -= 1
+
+    return None
+
+def send_video(ip_destino_cliente, porta_destino_cliente):
+    camera = CameraClient(ip_destino_cliente, porta_destino_cliente)
+    camera.start_stream()
+
+def start_streaming(server_socket, ip, port):
+    print("Iniciando streaming.")
+
+    response = aguardando_video_call(server_socket)
+    if response:
+        ip_destino, port_destino = response.split(',')
+
+        # Cliente espera receber a imagem do outro cliente
+        print(f"Server para receber video via vidstream: {ip}, {port}")
+        print(f"Server para enviar video via vidstream: {ip_destino}, {port_destino}")
+        server = StreamingServer(ip, port)
+        server.start_server()
+
+        # Envia a imagem para o outro cliente
+        send_video(ip_destino, int(port_destino))
+
+
+def main():
+    # Criação do socket do cliente
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Endereço do servidor
+    server_address = ("192.168.1.15", 5000)
+
+    # Conecta ao servidor
+    client_socket.connect(server_address)
+
+    while True:
+        response_from_server = client_socket.recv(4096).decode().split(',')
+        if response_from_server:
+            print(response_from_server[0])
+            ip_client = client_socket.getsockname()[0]
+            port_client = int(response_from_server[1])
+            start_streaming(client_socket, ip_client, port_client)
+
+if __name__ == "__main__":
+    main()
---
+++ entrega_2/server.py
@@ -0,0 +1,51 @@
+import socket
+import threading
+
+# Criação do socket do servidor
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Define o endereço e porta do servidor
+server_address = ('0.0.0.0', 5000)
+server_socket.bind(server_address)
+
+clients = []
+
+portas = [7007, 6001, 6002, 6003, 6004, 6005, 6006]
+
+portas_usadas = []
+
+def handle_client(client_socket):
+    print("Conexão de cliente", client_address)
+    porta = portas[len(portas) - 1]
+    portas_usadas.append(porta)
+    portas.remove(porta)
+    client_socket.send(f"Conexão estabelecida, {porta}".encode())
+    portas.pop(0)
+    clients.append(client_socket)
+
+    if len(clients) == 2:
+        client1 = clients[0]
+        client2 = clients[1]
+
+        # Envia a informação do cliente 1 para o cliente 2
+        client1_ip = client1.getpeername()[0]
+        client1_port = portas_usadas[0]
+        client2.send(f"{client1_ip},{client1_port}".encode())
+
+        # Envia a informação do cliente 2 para o cliente 1
+        client2_ip = client2.getpeername()[0]
+        client2_port = portas_usadas[1]
+        client1.send(f"{client2_ip},{client2_port}".encode())
+
+# Começa a ouvir por conexões
+server_socket.listen(5)
+print("Servidor de Videoconferência iniciado.")
+
+while True:
+    print("Aguardando conexões...")
+    client_socket, client_address = server_socket.accept()
+    print("Conexão de cliente", client_address)
+
+    #Inicia uma nova thread para cada cliente
+    client_handler = threading.Thread(target=handle_client, args=(client_socket,))
+    client_handler.start()
