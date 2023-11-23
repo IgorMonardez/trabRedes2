621c70b4b9056fb79ea7367c78ab3533996c654b
caiovelp <cparanhos@id.uff.br>
2023-11-23 15:12:55 -0300

    Modificações na pasta teste: teste 1

--- teste/client.py
+++ teste/client.py
@@ -1,9 +1,42 @@
+import socket
 from vidstream import CameraClient
-import threading
+import cv2
 
-# Create a camera client
-client = CameraClient('192.168.1.15', 9990)
-client.start_stream()
+def start_streaming(client_socket):
+    print("Iniciando streaming.")
 
-# The client is now running in a separate thread.
-# The client will capture video from the camera and send it to the server.
+    ip, port = client_socket.getpeername()
+
+    camera_client = CameraClient(ip, int(port))
+
+    cam = cv2.VideoCapture(0)
+
+    while True:
+        ret, frame = cam.read()
+
+        # Display the frame
+        cv2.imshow('Video Call', frame)
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+
+
+
+def main():
+    # Criação do socket do cliente
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Endereço do servidor
+    server_address = ("192.168.1.15", 7000)
+
+    # Conecta ao servidor
+    client_socket.connect(server_address)
+
+    while True:
+        response_from_server = client_socket.recv(4096).decode()
+        if response_from_server:
+            print(response_from_server)
+            start_streaming(client_socket)
+
+if __name__ == "__main__":
+    main()
--- teste/server.py
+++ teste/server.py
@@ -1,9 +1,30 @@
-from vidstream import StreamingServer
+import socket
 import threading
+from vidstream import StreamingServer
 
-# Create a streaming server
-server = StreamingServer('0.0.0.0', 9990)
-server.start_server()
+# Criação do socket do servidor
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# The server is now running in a separate thread.
-# The server will receive video from the clients and send it to all connected clients.
+# Define o endereço e porta do servidor
+server_address = ('0.0.0.0', 7000)
+server_socket.bind(server_address)
+
+clients = []
+
+def handle_client(client_socket):
+    print("Conexão de cliente", client_address)
+    client_socket.send("Conexão estabelecida".encode())
+    clients.append(client_socket)
+
+# Começa a ouvir por conexões
+server_socket.listen(5)
+print("Servidor de Videoconferência iniciado.")
+
+while True:
+    print("Aguardando conexões...")
+    client_socket, client_address = server_socket.accept()
+    print("Conexão de cliente", client_address)
+
+    #Inicia uma nova thread para cada cliente
+    client_handler = threading.Thread(target=handle_client, args=(client_socket,))
+    client_handler.start()
