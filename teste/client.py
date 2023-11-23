024f130739dc35bb5261b2fa0b7062ac3897cfdc
caiovelp <cparanhos@id.uff.br>
2023-11-23 13:07:04 -0300

    Primeiro teste com somente a lógica de vídeo

--- teste/client.py
+++ teste/client.py
@@ -1,11 +1,42 @@
-# client.py
-from vidstream import CameraClient
-import threading
+import pickle
+import socket
+import cv2
+import numpy as np
+import pyaudio
+import struct
 
-client = CameraClient('localhost', 9999)
+# Create a socket client
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect(('192.168.1.15', 7000))
 
-t = threading.Thread(target=client.start_stream)
-t.start()
+# Open the webcam and start capturing video
+cap = cv2.VideoCapture(0)
+
+# Open the microphone and start capturing audio
+p = pyaudio.PyAudio()
+stream = p.open(format=pyaudio.paInt16, channels=1, rate=44100, input=True, frames_per_buffer=1024)
 
 while True:
-    pass
+    # Capture a video frame
+    ret, frame = cap.read()
+
+    # Capture an audio frame
+    audio_frame = stream.read(1024)
+
+    # Serialize the video and audio frames
+    video_data = pickle.dumps(frame)
+    audio_data = pickle.dumps(audio_frame)
+
+    # Send the length of the serialized video frame
+    client_socket.sendall(struct.pack("L", len(video_data)))
+
+    # Send the serialized video frame in chunks
+    for i in range(0, len(video_data), 4096):
+        client_socket.sendall(video_data[i:i + 4096])
+
+    # Send the length of the serialized audio frame
+    client_socket.sendall(struct.pack("L", len(audio_data)))
+
+    # Send the serialized audio frame in chunks
+    for i in range(0, len(audio_data), 4096):
+        client_socket.sendall(audio_data[i:i + 4096])
--- teste/server.py
+++ teste/server.py
@@ -1,11 +1,54 @@
-# server.py
-from vidstream import StreamingServer
+import pickle
+import socket
 import threading
+import cv2
+import numpy as np
+import pyaudio
+import struct
 
-server = StreamingServer('localhost', 9999)
+# Create a socket server
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind(('0.0.0.0', 7000))
+server_socket.listen(5)
 
-t = threading.Thread(target=server.start_server)
-t.start()
+def handle_client(client_socket):
+    try:
+        while True:
+            # Receive the length of the serialized frame
+            data_len = struct.unpack("L", client_socket.recv(struct.calcsize("L")))[0]
+
+            # Receive the serialized frame in chunks
+            data = b""
+            while len(data) < data_len:
+                packet = client_socket.recv(min(data_len - len(data), 4096))
+                if not packet:
+                    return None
+                data += packet
+
+            # Check if all data has been received
+            if len(data) != data_len:
+                print("Data not fully received. Waiting for more data...")
+                continue
+
+            # Deserialize the frame
+            frame = pickle.loads(data)
+
+            # Display the frame
+            cv2.imshow('Received Video', frame)
+
+            if cv2.waitKey(1) == ord('q'):
+                break
+
+    except Exception as e:
+        print(f"Error while handling client: {e}")
+
+    finally:
+        cv2.destroyAllWindows()
+        client_socket.close()
+        print("Client handling finished.")
 
 while True:
-    pass
+    # Accept a new client and handle it in a new thread
+    client_socket, client_address = server_socket.accept()
+    client_thread = threading.Thread(target=handle_client, args=(client_socket,))
+    client_thread.start()
