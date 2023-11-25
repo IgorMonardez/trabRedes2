840eb712f6441100653b987817da8065a2bce731
caiovelp <cparanhos@id.uff.br>
2023-11-25 00:22:02 -0300

    Código refatorado parte 3.0 - Função de solicitar video chamada realizada - teste 2

--- teste/client.py
+++ teste/client.py
@@ -54,7 +54,7 @@ def main():
             user_response_accepted, user_ip_server_to_connect, user_port_server_to_connect = request_video_call(client_socket, user_to_call)
             if user_response_accepted:
                 print("Video chamada aceita.")
-                ip_server_to_host_connection = client_socket.getpeername()[0]
+                ip_server_to_host_connection = client_socket.getsockname()[0]
                 port_server_to_host_connection = porta_receber_chamadas
                 start_streaming(ip_server_to_host_connection, port_server_to_host_connection, user_ip_server_to_connect, user_port_server_to_connect)
         elif choice == "4":
@@ -62,13 +62,13 @@ def main():
             user_response_accepted, user_ip_server_to_connect, user_port_server_to_connect = waiting_request_video_call(client_socket, porta_receber_chamadas)
             if user_response_accepted:
                 print("Video chamada aceita.")
-                ip_server_to_host_connection = client_socket.getpeername()[0]
+                ip_server_to_host_connection = client_socket.getsockname()[0]
                 port_server_to_host_connection = porta_receber_chamadas
                 start_streaming(ip_server_to_host_connection, port_server_to_host_connection, user_ip_server_to_connect, user_port_server_to_connect)
-        elif choice == "5":
-            ip_receive_cam = client_socket.getsockname()[0]
-            port_receive_cam = int(porta_receber_chamadas)
-            start_streaming(client_socket, ip_receive_cam, port_receive_cam)
+        # elif choice == "5":
+        #     ip_receive_cam = client_socket.getsockname()[0]
+        #     port_receive_cam = int(porta_receber_chamadas)
+        #     start_streaming(client_socket, ip_receive_cam, port_receive_cam)
         elif choice == "6":
             unregister_success = quit_server(client_socket, client_name)
             if unregister_success:
